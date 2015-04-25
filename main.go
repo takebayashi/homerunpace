1a1c7ebc8b32dd2adfdc883b0822598acd4fdf8f
Shun Takebayashi <shun@takebayashi.asia>
2015-04-25 22:29:29 +0900

    Add Redis connection poolong

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 var dsn string
@@ -19,12 +20,14 @@ var updateToken string
 
 var redisNetwork string
 var redisAddress string
+var redisPassword string
 
 func main() {
 	dsn = os.Getenv("HOMERUNRATE_DSN")
 	updateToken = os.Getenv("HOMERUNERATE_TOKEN")
 	redisNetwork = os.Getenv("HOMERUNRATE_REDIS_NETWORK")
 	redisAddress = os.Getenv("HOMERUNRATE_REDIS_ADDRESS")
+	redisPassword = os.Getenv("HOMERUNRATE_REDIS_PASSWORD")
 	root := os.Getenv("HOMERUNERATE_ROOT")
 	flag.Set("bind", ":80")
 	goji.Get("/stats/:year", handleStats)
@@ -33,16 +36,44 @@ func main() {
 	goji.Serve()
 }
 
+var redisPool *redis.Pool
+
+func newRedisConn() redis.Conn {
+	if redisPool == nil {
+		redisPool = &redis.Pool{
+			MaxIdle:     3,
+			IdleTimeout: 600 * time.Second,
+			Dial: func() (redis.Conn, error) {
+				c, err := redis.Dial(redisNetwork, redisAddress)
+				if err != nil {
+					return nil, err
+				}
+				if redisPassword != "" {
+					_, err = c.Do("AUTH", redisPassword)
+					if err != nil {
+						c.Close()
+						return nil, err
+					}
+				}
+				return c, err
+			},
+			TestOnBorrow: func(c redis.Conn, t time.Time) error {
+				_, err := c.Do("PING")
+				return err
+			},
+		}
+	}
+	return (*redisPool).Get()
+}
+
 func handleStats(c web.C, w http.ResponseWriter, r *http.Request) {
 	year, _ := strconv.Atoi(c.URLParams["year"])
 	by := r.FormValue("by")
 	key := strconv.Itoa(year) + by
 	var j []byte
 
-	rc, err := redis.Dial(redisNetwork, redisAddress)
-	if err != nil {
-		panic(err)
-	}
+	rc := newRedisConn()
+	defer rc.Close()
 
 	v, err := redis.String(rc.Do("GET", key))
 	if err != nil {
@@ -150,10 +181,8 @@ func handleCrawling(c web.C, w http.ResponseWriter, r *http.Request) {
 		crawl(date)
 		year, _ := strconv.Atoi(date[:4])
 
-		rc, err := redis.Dial(redisNetwork, redisAddress)
-		if err != nil {
-			panic(err)
-		}
+		rc := newRedisConn()
+		defer rc.Close()
 
 		for _, by := range []string{"date", "game"} {
 			key := strconv.Itoa(year) + by
